bc26c11af92bab0909179d9a24ad37df81ed6e1c
AngryPesel <47815331+AngryPesel@users.noreply.github.com>
2025-08-14 11:14:15 +0300

    Update main.py
    Death chances changed to 5%

--- main.py
+++ main.py
@@ -871,7 +871,7 @@ def handle_zonewalk(chat_id, user_id, username, arg_item):
 
         # NEW FEATURE: Моментальна смерть
         # 5% шанс моментальної смерті, з випадковим повідомленням
-        if random.random() < 0.99:
+        if random.random() < 0.05:
             kill_pet(chat_id, player_data['user_id'])
             death_title = "☠️Ще одне пацєтко поглинула Зона...☠️"
             death_text = random.choice(death_messages)
